c4cc61f8240bf6b0b5119b5f588c6f2959dc176e
ONEOWNWORK10\ozono-yu-ca <vocaloid398@gmail.com>
2018-06-30 23:18:41 +0900

    :+1: パターン追加

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,59 +1,85 @@
-
+// パターン02
 
 function extendLink(){
 
-var locatonList = new Array(); 
-locatonList['aa:b1:ca:d1']='http://www.google.co.jp/aab1cad1'; 
-locatonList['aa:b1:ca:d2']='http://www.google.co.jp/aab1cad2'; 
-locatonList['aa:b1:cb:d1']='http://www.google.co.jp/aab1cbd1'; 
-locatonList['aa:b1:cb:d2']='http://www.google.co.jp/aab1cbd2'; 
+	var locatonList = new Array(); 
+	locatonList['aa:b1:ca:d1']='http://www.google.co.jp/aab1cad1'; 
+	locatonList['aa:b1:ca:d2']='http://www.google.co.jp/aab1cad2'; 
+	locatonList['aa:b1:cb:d1']='http://www.google.co.jp/aab1cbd1'; 
+	locatonList['aa:b1:cb:d2']='http://www.google.co.jp/aab1cbd2'; 
 
-locatonList['aa:b2:ca:d1']='http://www.google.co.jp/aab2cad1'; 
-locatonList['aa:b2:ca:d2']='http://www.google.co.jp/aab2cad2'; 
-locatonList['aa:b2:cb:d1']='http://www.google.co.jp/aab2cbd1'; 
-locatonList['aa:b2:cb:d2']='http://www.google.co.jp/aab2cbd2'; 
+	locatonList['aa:b2:ca:d1']='http://www.google.co.jp/aab2cad1'; 
+	locatonList['aa:b2:ca:d2']='http://www.google.co.jp/aab2cad2'; 
+	locatonList['aa:b2:cb:d1']='http://www.google.co.jp/aab2cbd1'; 
+	locatonList['aa:b2:cb:d2']='http://www.google.co.jp/aab2cbd2'; 
 
-locatonList['ab:b1:ca:d1']='http://www.google.co.jp/abb1cad1'; 
-locatonList['ab:b1:ca:d2']='http://www.google.co.jp/abb1cad2'; 
-locatonList['ab:b1:cb:d1']='http://www.google.co.jp/abb1cbd1'; 
-locatonList['ab:b1:cb:d2']='http://www.google.co.jp/abb1cbd2'; 
+	locatonList['ab:b1:ca:d1']='http://www.google.co.jp/abb1cad1'; 
+	locatonList['ab:b1:ca:d2']='http://www.google.co.jp/abb1cad2'; 
+	locatonList['ab:b1:cb:d1']='http://www.google.co.jp/abb1cbd1'; 
+	locatonList['ab:b1:cb:d2']='http://www.google.co.jp/abb1cbd2'; 
 
-locatonList['ab:b2:ca:d1']='http://www.google.co.jp/abb2cad1'; 
-locatonList['ab:b2:ca:d2']='http://www.google.co.jp/abb2cad2'; 
-locatonList['ab:b2:cb:d1']='http://www.google.co.jp/abb2cbd1'; 
-locatonList['ab:b2:cb:d2']='http://www.google.co.jp/abb2cbd2'; 
+	locatonList['ab:b2:ca:d1']='http://www.google.co.jp/abb2cad1'; 
+	locatonList['ab:b2:ca:d2']='http://www.google.co.jp/abb2cad2'; 
+	locatonList['ab:b2:cb:d1']='http://www.google.co.jp/abb2cbd1'; 
+	locatonList['ab:b2:cb:d2']='http://www.google.co.jp/abb2cbd2'; 
 
-var key = ''; 
+	var key = ''; 
 
-var groupA = document.getElementsByName('groupA'); 
-for(var i=0;i<groupA.length;i++){ 
-if(groupA[i].checked) { 
-key = groupA[i].value + ':'; 
-break; 
+	var groupA = document.getElementsByName('groupA'); 
+	for(var i=0;i<groupA.length;i++){ 
+		if(groupA[i].checked) { 
+		key = groupA[i].value + ':'; 
+		break; 
+		} 
+	} 
+	var groupB = document.getElementsByName('groupB'); 
+	for(var i=0;i<groupB.length;i++){ 
+		if(groupB[i].checked) { 
+		key = key + groupB[i].value + ':'; 
+		break; 
+		} 
+	}
+	var groupC = document.getElementsByName('groupC'); 
+	for(var i=0;i<groupA.length;i++){ 
+		if(groupC[i].checked) { 
+		key = key + groupC[i].value + ':'; 
+		break; 
+		} 
+	} 
+	var groupD = document.getElementsByName('groupD'); 
+	for(var i=0;i<groupD.length;i++){ 
+		if(groupD[i].checked) { 
+		key = key + groupD[i].value; 
+		break; 
+		} 
+	}
+	document.getElementById('variableLink').href=locatonList[key]; 
+	document.getElementById('variableLink').firstChild.nodeValue='['+key+']'; 
 } 
-} 
-var groupB = document.getElementsByName('groupB'); 
-for(var i=0;i<groupB.length;i++){ 
-if(groupB[i].checked) { 
-key = key + groupB[i].value + ':'; 
-break; 
-} 
+
+// パターン03
+function sample(F){
+	var list = {
+		0:'htp://example.com/a.html',//0以下
+		2:'htp://example.com/b.html',//2以下
+		5:'htp://example.com/c.html',
+		8:'htp://example.com/d.html',
+		10:'htp://example.com/e.html'
+	};
+
+	var i=0;
+	var q;
+	var c=0;
+	var key=0;
+
+	while( q = F.elements[ i++ ] )
+	if(q.type=='radio' && q.checked) c+= parseInt(q.value);
+
+	for(key in list) {
+		if(c <= parseInt(key)) {
+		location.href = list[key];
+		break;
+		}
+	}
+	return false;
 }
-var groupC = document.getElementsByName('groupC'); 
-for(var i=0;i<groupA.length;i++){ 
-if(groupC[i].checked) { 
-key = key + groupC[i].value + ':'; 
-break; 
-} 
-} 
-var groupD = document.getElementsByName('groupD'); 
-for(var i=0;i<groupD.length;i++){ 
-if(groupD[i].checked) { 
-key = key + groupD[i].value; 
-break; 
-} 
-}
-document.getElementById('variableLink').href=locatonList[key]; 
-document.getElementById('variableLink').firstChild.nodeValue='['+key+']'; 
-
-} 
