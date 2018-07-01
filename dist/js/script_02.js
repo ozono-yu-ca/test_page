ac8053635f222c49497e8a01fb11d68530ca1d26
ozono-yu-ca <vocaloid398@gmail.com>
2018-07-01 11:50:55 +0900

    :+1: ãã¿ã¼ã³02ä¿®æ­£

--- dist/js/script_02.js
+++ dist/js/script_02.js
@@ -3,39 +3,54 @@
 function extendLink(){
 
 	var locatonList = new Array(); 
-	locatonList['aaa:bbb:ca:d1']='http://www.google.co.jp/aaab1cad1'; 
-	locatonList['aaa:111:ca:d2']='http://www.google.co.jp/aaab1cad2'; 
-	locatonList['aaa:222:cb:d1']='http://www.google.co.jp/aaab1cbd1'; 
-	locatonList['aaa:b1:cb:d2']='http://www.google.co.jp/aaab1cbd2'; 
 
-	locatonList['aa:b2:ca:d1']='http://www.google.co.jp/aab2cad1'; 
-	locatonList['aa:b2:ca:d2']='http://www.google.co.jp/aab2cad2'; 
-	locatonList['aa:b2:cb:d1']='http://www.google.co.jp/aab2cbd1'; 
-	locatonList['aa:b2:cb:d2']='http://www.google.co.jp/aab2cbd2'; 
+	locatonList['aaa']='http://www.google.co.jp/aaa'; 
+	locatonList['aaabbb']='http://www.google.co.jp/aaabbb'; 
+	locatonList['aaabbb111']='http://www.google.co.jp/aaabbb111'; 
+	locatonList['aaabbb111222']='http://www.google.co.jp/aaabbb111222'; 
+	locatonList['aaabbb222']='http://www.google.co.jp/aaabbb222'; 
+	locatonList['aaa111']='http://www.google.co.jp/aaa111'; 
+	locatonList['aaa111222']='http://www.google.co.jp/aaa111222'; 
+	locatonList['aaa222']='http://www.google.co.jp/aaa222'; 
 
-	locatonList['ab:b1:ca:d1']='http://www.google.co.jp/abb1cad1'; 
-	locatonList['ab:b1:ca:d2']='http://www.google.co.jp/abb1cad2'; 
-	locatonList['ab:b1:cb:d1']='http://www.google.co.jp/abb1cbd1'; 
-	locatonList['ab:b1:cb:d2']='http://www.google.co.jp/abb1cbd2'; 
+	locatonList['bbb']='http://www.google.co.jp/bbb'; 
+	locatonList['bbb111']='http://www.google.co.jp/bbb111'; 
+	locatonList['bbb111222']='http://www.google.co.jp/bbb111222'; 
+	locatonList['bbb222']='http://www.google.co.jp/bbb222'; 
 
-	locatonList['ab:b2:ca:d1']='http://www.google.co.jp/abb2cad1'; 
-	locatonList['ab:b2:ca:d2']='http://www.google.co.jp/abb2cad2'; 
-	locatonList['ab:b2:cb:d1']='http://www.google.co.jp/abb2cbd1'; 
-	locatonList['ab:b2:cb:d2']='http://www.google.co.jp/abb2cbd2'; 
+	locatonList['111']='http://www.google.co.jp/111'; 
+	locatonList['111222']='http://www.google.co.jp/111222'; 
+
+	locatonList['222']='http://www.google.co.jp/222'; 
+
 
 	var key = ''; 
 
-	var groupA = document.getElementsByName('groupA'); 
-	for(var i=0;i<groupA.length;i++){ 
-		if(groupA[i].checked) { 
-		key = groupA[i].value + ':'; 
+	var groupA_1 = document.getElementsByName('groupA-1'); 
+	for(var i=0;i<groupA_1.length;i++){ 
+		if(groupA_1[i].checked) { 
+		key = key + groupA_1[i].value; 
+		break; 
+		} 
+	}
+	var groupA_2 = document.getElementsByName('groupA-2'); 
+	for(var i=0;i<groupA_2.length;i++){ 
+		if(groupA_2[i].checked) { 
+		key = key + groupA_2[i].value; 
 		break; 
 		} 
 	} 
-	var groupB = document.getElementsByName('groupB'); 
-	for(var i=0;i<groupB.length;i++){ 
-		if(groupB[i].checked) { 
-		key = key + groupB[i].value + ':'; 
+	var groupB_1 = document.getElementsByName('groupB-1'); 
+	for(var i=0;i<groupB_1.length;i++){ 
+		if(groupB_1[i].checked) { 
+		key = key + groupB_1[i].value; 
+		break; 
+		} 
+	}
+	var groupB_2 = document.getElementsByName('groupB-2'); 
+	for(var i=0;i<groupB_2.length;i++){ 
+		if(groupB_2[i].checked) { 
+		key = key + groupB_2[i].value; 
 		break; 
 		} 
 	}
